079cb6e50bab2ccb961c196e91e792301e7fadfb
SoundhariKumar <soundharikumar8@gmail.com>
2024-08-27 23:29:01 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "student-management-api",
+  "version": "1.0.0",
+  "description": "A RESTful API for managing students and departments.",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"
+  },
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2",
+    "mongoose": "^7.5.0"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.22"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  },
+  "author": "Your Name",
+  "license": "ISC"
+}
